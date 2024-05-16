5bf7fb92db90d8373680144758696e424283def1
Andriy Beha <apgroup.ua@gmail.com>
2024-05-16 11:52:43 +0200

    Task completed

--- src/main/kotlin/mate/academy/service/PasswordValidator.kt
+++ src/main/kotlin/mate/academy/service/PasswordValidator.kt
@@ -1,8 +1,15 @@
 package mate.academy.service
 
 // This class will validate password requirements
+import mate.academy.exception.PasswordValidationException
+
+private const val REQUIRED_PASSWORD_LENGTH = 10
+
 class PasswordValidator {
     fun validate(password: String, repeatPassword: String) {
         // write your code here
+        if (password != repeatPassword || password.length < REQUIRED_PASSWORD_LENGTH) {
+            throw PasswordValidationException("Wrong passwords")
+        }
     }
 }
