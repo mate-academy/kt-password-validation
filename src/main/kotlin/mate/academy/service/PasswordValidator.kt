a23f0f0b4d65dfcc3dbd59613ef551a8c175f192
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-07-05 09:37:25 +0300

    added hw

--- src/main/kotlin/mate/academy/service/PasswordValidator.kt
+++ src/main/kotlin/mate/academy/service/PasswordValidator.kt
@@ -1,8 +1,12 @@
 package mate.academy.service
 
-// This class will validate password requirements
+import mate.academy.exception.PasswordValidationException
+
+const val PASSWORD_INCORRECT_LENGTH = 10
 class PasswordValidator {
     fun validate(password: String, repeatPassword: String) {
-        // write your code here
+        if (password.length < PASSWORD_INCORRECT_LENGTH || password != repeatPassword) {
+            throw PasswordValidationException("Wrong passwords")
+        }
     }
 }
--- src/main/kotlin/mate/academy/service/UserService.kt
+++ src/main/kotlin/mate/academy/service/UserService.kt
@@ -1,5 +1,6 @@
 package mate.academy.service
 
+import mate.academy.exception.PasswordValidationException
 import mate.academy.model.User
 
 // This class represents a user service with user registration functionality
@@ -11,6 +12,13 @@ class UserService {
     }
 
     fun registerUser(username: String, password: String, repeatPassword: String) : String {
-
+        val passwordValidator = PasswordValidator()
+       return try {
+           passwordValidator.validate(password, repeatPassword)
+           val user = User(username, password)
+           saveUser(user)
+       } catch (e: PasswordValidationException) {
+           "Your passwords are incorrect. Try again."
+       }
     }
 }
