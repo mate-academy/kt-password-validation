e6737497d52079579e8ec3f74397ac33c4df823c
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-07-19 11:20:54 +0300

    feat [kotlin]: implement password validation

--- src/main/kotlin/mate/academy/service/UserService.kt
+++ src/main/kotlin/mate/academy/service/UserService.kt
@@ -1,5 +1,6 @@
 package mate.academy.service
 
+import mate.academy.exception.PasswordValidationException
 import mate.academy.model.User
 
 // This class represents a user service with user registration functionality
@@ -11,6 +12,11 @@ class UserService {
     }
 
     fun registerUser(username: String, password: String, repeatPassword: String) : String {
-
+        try {
+            PasswordValidator().validate(password, repeatPassword)
+        } catch (exception: PasswordValidationException) {
+            return "Your passwords are incorrect. Try again."
+        }
+        return saveUser(User(username, password))
     }
 }
