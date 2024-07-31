13370b2a5ebe73b2982a9528af141fb7e3766986
Viktor Kushnir <viktor.kushnir404@gmail.com>
2024-07-31 14:23:12 +0300

    Password validation solution

--- src/main/kotlin/mate/academy/service/UserService.kt
+++ src/main/kotlin/mate/academy/service/UserService.kt
@@ -1,16 +1,23 @@
 package mate.academy.service
 
+import mate.academy.exception.PasswordValidationException
 import mate.academy.model.User
 
 // This class represents a user service with user registration functionality
 class UserService {
+    private val passwordValidator = PasswordValidator()
 
-    fun saveUser(user: User) : String {
+    private fun saveUser(user: User) : String {
         // This is where you would typically save the user to a database
         return "User ${user.username} saved successfully."
     }
 
-    fun registerUser(username: String, password: String, repeatPassword: String) : String {
-
+    fun registerUser(username: String, password: String, repeatPassword: String): String {
+        try {
+            passwordValidator.validate(password, repeatPassword)
+            return saveUser(User(username, password))
+        } catch (e: PasswordValidationException) {
+            return "Your passwords are incorrect. Try again."
+        }
     }
 }
