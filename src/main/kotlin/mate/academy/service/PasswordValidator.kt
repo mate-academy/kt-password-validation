a50fd7548181e8b29308beb3d4f1ec71352f8fe3
Inna Diachkina <innadyachkina@gmail.com>
2024-09-13 08:25:49 +0300

    fixed mistake

--- src/main/kotlin/mate/academy/service/PasswordValidator.kt
+++ src/main/kotlin/mate/academy/service/PasswordValidator.kt
@@ -6,7 +6,7 @@ const val MIN_PASSWORD_LENGTH = 10;
 class PasswordValidator {
     fun validate(password: String, repeatPassword: String) {
         if (password != repeatPassword || password.length < MIN_PASSWORD_LENGTH) {
-            throw PasswordValidationException("Wrong passwords.")
+            throw PasswordValidationException("Wrong passwords")
         }
     }
 }
