4c55043b11bf411e2a4312a3c60fad2d02454118
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-10-05 10:27:02 +0300

    Removed long string

--- src/main/kotlin/mate/academy/service/PasswordValidator.kt
+++ src/main/kotlin/mate/academy/service/PasswordValidator.kt
@@ -8,7 +8,8 @@ private const val DEFAULT_LENGTH = 10
 class PasswordValidator {
     @Throws(PasswordValidationException::class)
     fun validate(password: String, repeatPassword: String) {
-        if ((password.length < DEFAULT_LENGTH || repeatPassword.length < DEFAULT_LENGTH) || password != repeatPassword) {
+        if ((password.length < DEFAULT_LENGTH || repeatPassword.length < DEFAULT_LENGTH)
+            || password != repeatPassword) {
             throw PasswordValidationException("Wrong passwords")
         }
     }
