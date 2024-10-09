e7de94f7d27f82ac59ee7b942905a5064a5647d4
Oleksand Kolisnyk <kolisnykol@gmail.com>
2024-10-09 20:37:37 +0300

    PasswordValidator - Declaring that this method can throw a checked exception

--- src/main/kotlin/mate/academy/service/PasswordValidator.kt
+++ src/main/kotlin/mate/academy/service/PasswordValidator.kt
@@ -6,6 +6,8 @@ const val PASSWORD_MIN_LENGTH = 10
 
 // This class will validate password requirements
 class PasswordValidator {
+    // Declaring that this method can throw a checked exception
+    @Throws(PasswordValidationException::class)
     fun validate(password: String, repeatPassword: String) {
         // write your code here
         if (password.length < PASSWORD_MIN_LENGTH ||
