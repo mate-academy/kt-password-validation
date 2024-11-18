24ea62fe62c46d201eb85accc7518976a9974825
Danylo Kishchenko <amfich@gmail.com>
2024-11-19 00:22:00 +0200

    added 'PasswordValidationException' class; implemented fun 'registerUser()' in the 'UserService' class; implemented fun 'validate()' in the 'PasswordValidator' class.

--- src/main/kotlin/mate/academy/service/PasswordValidator.kt
+++ src/main/kotlin/mate/academy/service/PasswordValidator.kt
@@ -1,8 +1,16 @@
 package mate.academy.service
 
+import mate.academy.exception.PasswordValidationException
+
 // This class will validate password requirements
 class PasswordValidator {
+    companion object {
+        const val MIN_PASSWORD_LENGTH = 10
+    }
+
     fun validate(password: String, repeatPassword: String) {
-        // write your code here
+        require(password.length >= MIN_PASSWORD_LENGTH && password == repeatPassword) {
+            throw PasswordValidationException("Wrong passwords")
+        }
     }
 }
