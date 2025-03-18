22d39fd9ccaa6fdcdd0a75ccdc2fbe239aea5822
Roman Voynahiy <romanvoynahiy@gmail.com>
2025-03-18 16:08:59 +0200

    Implement registerUser() and validate() methods with custom PasswordValidationException for password validation.

--- src/main/kotlin/mate/academy/service/UserService.kt
+++ src/main/kotlin/mate/academy/service/UserService.kt
@@ -1,6 +1,8 @@
 package mate.academy.service
 
+import mate.academy.exception.PasswordValidationException
 import mate.academy.model.User
+import mate.academy.service.PasswordValidator
 
 // This class represents a user service with user registration functionality
 class UserService {
@@ -11,6 +13,13 @@ class UserService {
     }
 
     fun registerUser(username: String, password: String, repeatPassword: String) : String {
-
+        val validator = PasswordValidator()
+        try {
+            validator.validate(password, repeatPassword)
+            val user = User(username, password)
+            return saveUser(user)
+        } catch (e: PasswordValidationException) {
+            return "Your passwords are incorrect. Try again."
+        }
     }
 }
